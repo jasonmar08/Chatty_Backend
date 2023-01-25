cd442330db3ae4e7b4b8d7ad72d3d1ecbe698a25
Jason Martinez <jasonmar08@gmail.com>
2023-01-25 05:05:37 -0500

    set up backend boilerplate and server

---
+++ server.js
@@ -0,0 +1,32 @@
+import 'dotenv/config.js'
+import db from './db/connection.js'
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
+import routes from './routes/index.js'
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+app.use(routes)
+
+db.on('connected', () => {
+  console.log('Connected to MongoDB!')
+  app
+    .listen(PORT, () => {
+      console.log(`Express server application is running on port: ${PORT}\n\n`)
+    })
+    .on('error', (err) => {
+      console.error(`Express server error: ${err}`)
+      process.exit(1)
+    })
+})
+
+// Listen for the 'error' event emitted by the MongoDB driver
+db.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err}`)
+  process.exit(1)
+})
