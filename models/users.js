5ea36cdb8657f5e754f4e3b4f554c8a281d209f0
Jason Martinez <jasonmar08@gmail.com>
2023-01-26 02:32:15 -0500

    create user schema

---
+++ models/users.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const user = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      match: /.+\@.+\..+/,
+      unique: [true, 'This email already exists.']
+    },
+    passwordDigest: { type: String, required: true },
+    phoneNumber: {
+      type: String,
+      match: /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$|^\d{10}$/
+    },
+    profilePicture: { type: String },
+    verified: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('User', user)
