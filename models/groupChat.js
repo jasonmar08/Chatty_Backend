95e1dfed0ce50771d42022909b8033ce558d3007
Jason Martinez <jasonmar08@gmail.com>
2023-01-27 15:47:29 -0500

    add timestamps property to privateChat and groupChat schema

--- models/groupChat.js
+++ models/groupChat.js
@@ -1,39 +1,42 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
-const groupChat = new Schema({
-  groupName: { type: String, required: true },
-  groupDescription: { type: String, default: 'Group' },
-  groupPhoto: { type: String },
-  participants: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: 'User',
-      isAdmin: { type: Boolean, default: false, required: true }
-    }
-  ],
-  creator: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
-  typingUsers: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
-  messages: [
-    {
-      text: { type: String },
-      sender: { type: mongoose.Types.ObjectId, ref: 'User' },
-      timestamp: { type: Date, default: Date.now },
-      status: {
-        type: String,
-        enum: ['sent', 'read', 'failed'],
-        default: 'sent'
-      },
-      readBy: [
-        {
-          user: { type: mongoose.Types.ObjectId, ref: 'User' },
-          timestamp: { type: Date, default: Date.now }
-        }
-      ]
-    }
-  ],
-  lastActive: { type: Date, default: Date.now }
-})
+const groupChat = new Schema(
+  {
+    groupName: { type: String, required: true },
+    groupDescription: { type: String, default: 'Group' },
+    groupPhoto: { type: String },
+    participants: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        isAdmin: { type: Boolean, default: false, required: true }
+      }
+    ],
+    creator: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    typingUsers: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
+    messages: [
+      {
+        text: { type: String },
+        sender: { type: mongoose.Types.ObjectId, ref: 'User' },
+        timestamp: { type: Date, default: Date.now },
+        status: {
+          type: String,
+          enum: ['sent', 'read', 'failed'],
+          default: 'sent'
+        },
+        readBy: [
+          {
+            user: { type: mongoose.Types.ObjectId, ref: 'User' },
+            timestamp: { type: Date, default: Date.now }
+          }
+        ]
+      }
+    ],
+    lastActive: { type: Date, default: Date.now }
+  },
+  { timestamps: true }
+)
 
 groupChat.index({ groupName: 1, 'participants._id': 1 }, { unique: true })
 
