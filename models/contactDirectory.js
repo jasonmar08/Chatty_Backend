5283aaa268fb226c46ad542e83b0fcc15b7522ee
Jason Martinez <jasonmar08@gmail.com>
2023-01-28 03:06:46 -0500

    set up contactDirectory model

--- models/contactDirectory.js
+++ models/contactDirectory.js
@@ -2,25 +2,25 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const contactDirectory = new Schema(
-  [
-    {
-      contactFirstName: { type: String, required: true },
-      contactLastName: { type: String, default: '' },
-      contactEmail: {
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User' },
+    contact: {
+      firstName: { type: String, required: true },
+      lastName: { type: String, default: '' },
+      email: {
         type: String,
-        default: '',
         match: /.+\@.+\..+/,
         unique: true,
         index: true,
         required: true
       },
-      contactPhoneNumber: {
+      phoneNumber: {
         type: String,
         match: /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$|^\d{10}$/
       },
       isFavorite: { type: Boolean, default: false }
     }
-  ],
+  },
   { timestamps: true }
 )
 
