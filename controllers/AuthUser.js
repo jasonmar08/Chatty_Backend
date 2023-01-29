3b8ae13c404e13acbb0d7809088d184b9dadffd4
Jason Martinez <jasonmar08@gmail.com>
2023-01-29 03:45:13 -0500

    set up authUser controller

--- controllers/AuthUser.js
+++ controllers/AuthUser.js
@@ -30,10 +30,10 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
-    const { email, password } = req.body
-    const user = await User.findOne({ email })
+    const { email = email.toLowerCase(), password } = req.body
+    const user = await User.findOne({ email: email.toLowerCase() })
 
-    if (user && comparePassword(user.passwordDigest, password)) {
+    if (user && (await comparePassword(user.passwordDigest, password))) {
       let payload = {
         id: user._id,
         firstName: user.firstName,
@@ -48,4 +48,34 @@ export const login = async (req, res) => {
     console.error(error.message)
     res.status(400).json({ error: error.message })
   }
+}
+
+export const updatePassword = async (req, res) => {
+  try {
+    const { userId } = req.params
+    const user = await User.findById(userId)
+    if (
+      user &&
+      (await comparePassword(user.passwordDigest, req.body.oldPassword))
+    ) {
+      let passwordDigest = await hashPassword(req.body.newPassword)
+      await user.findByIdAndUpdate(userId, { passwordDigest }, { new: true })
+      return res.status(201).json({
+        status: 'Success',
+        message: 'Your password has been successfully updated.'
+      })
+    }
+    res
+      .status(401)
+      .json({ status: 'Error', message: 'Old password is incorrect.' })
+  } catch (error) {
+    console.log(error.message)
+    res.status(400).json({ error: error.message })
+  }
+}
+
+export const checkSession = async (req, res) => {
+  console.log(res.locals.payload)
+  const { payload } = res.locals
+  res.send(payload)
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,22 @@
 import { Router } from 'express'
-// import middleware from '../middleware/jwtAuth.js'
-import { login, register } from '../controllers/authUser.js'
+import { stripToken, verifyToken } from '../middleware/jwtAuth.js'
+import {
+  checkSession,
+  login,
+  register,
+  updatePassword
+} from '../controllers/authUser.js'
 const router = Router()
 
 // Auth
 router.post('/register', register)
 router.post('/login', login)
+router.patch(
+  '/users/:userId/update-password',
+  stripToken,
+  verifyToken,
+  updatePassword
+)
+router.get('/session', stripToken, verifyToken, checkSession)
 
 export default router
