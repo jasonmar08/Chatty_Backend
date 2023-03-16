df73a1caef90e08323d6df055600461cf8e0999f
Jason Martinez <jasonmar08@gmail.com>
2023-03-15 21:57:53 -0400

    initialize socket.io with server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import logger from 'morgan'
 import cors from 'cors'
 import routes from './routes/index.js'
+import { Server } from 'socket.io'
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -15,7 +16,7 @@ app.use(routes)
 
 db.on('connected', () => {
   console.log('Connected to MongoDB!')
-  app
+  const server = app
     .listen(PORT, () => {
       console.log(`Express server application is running on port: ${PORT}\n\n`)
     })
@@ -23,6 +24,15 @@ db.on('connected', () => {
       console.error(`Express server error: ${err}`)
       process.exit(1)
     })
+  const io = new Server(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: 'http://localhost:3001'
+    }
+  })
+  io.on('connection', (socket) => {
+    console.log('Connected to socket.io')
+  })
 })
 
 db.on('error', (err) => {
